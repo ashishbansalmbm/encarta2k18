1b546f55ef2f95783b739938e4da8dd0623fbdba
Ashish Bansal <ashishbansalmbm@gmail.com>
2018-11-28 23:37:15 +0530

    added filters for admin panel

--- user/admin.py
+++ user/admin.py
@@ -3,14 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from user.models import Profile, Event, Participation
 
-admin.site.register(Profile)
+
 admin.site.register(Event)
 
 
 class ParticipationAdmin(admin.ModelAdmin):
     list_display = ['name', 'en', 'event', 'cost', 'verified']
     list_editable = ['verified']
-    list_filter = ['user__first_name', 'event']
+    list_filter = ['verified', 'event', 'user__first_name']
+    search_fields = ['user__first_name', 'user__profile__encarta_id']
 
     def name(self, obj):
         return obj.user.first_name + ' ' + obj.user.last_name
@@ -21,6 +22,12 @@ class ParticipationAdmin(admin.ModelAdmin):
     def cost(self,obj):
         return obj.event.cost
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mobile', 'current_year_of_study', 'encarta_id']
+    search_fields = ['encarta_id']
 
+    def name(self, obj):
+        return obj.user.first_name + ' ' + obj.user.last_name
 
 admin.site.register(Participation, ParticipationAdmin)
+admin.site.register(Profile, ProfileAdmin)
